c441a2fc9454fc717ccc42fb3f2519a033565fbe
kr1sp1n <krispinone@googlemail.com>
2015-09-24 22:00:52 +0200

    add input to change title

--- app.js
+++ app.js
@@ -7,15 +7,40 @@ var h = require('mercury').h;
 
 // define internal state (analogous to a model in mvc)
 function App() {
-  return hg.state({
-    value: "Hello Mercury!"
+  var state = hg.struct({
+    title: hg.value('Mercury'),
+    handles: hg.value(null)
+  });
+
+  state.handles.set(hg.handles({
+    change: setTitle
+  }, state));
+
+  return state;
+}
+
+function setTitle(state, data) {
+  state.title.set(data.title);
+}
+
+function inputBox(value, sink) {
+  return h('input', {
+    value: value,
+    name: 'title',
+    type: 'text',
+    'ev-event': hg.changeEvent(sink)
   });
 }
 
 // render virtual-dom from the internal state
 App.render = function render(state){
-  // create a virtual-dom `h1` element containing "Hello Mercury!"
-  return h('h1', state.value);
+  return h('div', [
+    h('h1', 'Hello ' + state.title + '!'),
+    h('p', [
+        'Change it here: ',
+        inputBox(state.title, state.handles.change)
+    ])
+  ]);
 }
 
 // mount the virtual-dom on the html document body
